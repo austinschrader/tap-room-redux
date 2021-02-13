72f2153977da388956662ec13101fd1c1be38e1a
Austin Schrader <austin.jschrader@gmail.com>
2021-02-12 20:00:34 -0800

    Add keg control constructor

--- src/components/NewKegControl.js
+++ src/components/NewKegControl.js
@@ -4,6 +4,13 @@ import KegList from "./KegList";
 import KegDetail from "./KegDetail";
 
 class NewKegControl extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleView: 0,
+      masterKegList: [],
+      selectedKeg: null
+    };
+  }
 
 }
