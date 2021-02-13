42868b28283d554b455a72852b62908e476af56b
Austin Schrader <austin.jschrader@gmail.com>
2021-02-12 16:58:54 -0800

    Add keg list display

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,1 +1,18 @@
 import React from "react";
+import Keg from "./Keg";
+import PropTypes from "prop-types";
+
+function KegList(props) {
+  return ( 
+    <>
+      <hr/>
+    </>
+  )
+}
+
+KegList.propsTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+}
+
+export default KegList;
