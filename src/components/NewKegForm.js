8381b580f1fbde934e0bade13d8a172ed738f1db
Austin Schrader <austin.jschrader@gmail.com>
2021-02-12 17:00:45 -0800

    Add display to newkegform

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,1 +1,23 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+
+function NewKegForm(props) {
+  return (
+    <>
+      <form onSubmt={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Keg name'
+          required />
+      </form>
+    </>
+  )
+};
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
