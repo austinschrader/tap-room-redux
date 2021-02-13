003e3bb011367d643a518e79ea59481ddf825ead
Austin Schrader <austin.jschrader@gmail.com>
2021-02-12 16:39:35 -0800

    Add prop types to Keg

---
+++ src/components/Keg.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  alcoholContent: PropTypes.number.isRequired
+}
