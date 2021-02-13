84db9985ca154f126a09d91825ba472711441c14
Austin Schrader <austin.jschrader@gmail.com>
2021-02-12 16:54:43 -0800

    Add keg output

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,12 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+function Keg(props) {
+  let stock = props.stock;
 
+    <>
+      return (
+        <div onClick = {() => props.kegClicked(props.id)}>
+          <h4>{props.name}</h4>
+          <h4>{props.brand}</h4>
+          <h4>{props.price}</h4>
+          <h4>{props.alcoholContent}</h4>
+          <h4>{props.stock}</h4>
+          <h4>{props.id}</h4>
+        </div>
+     </>
+    )
+  }
+}
 
 
 Keg.propTypes = {
   name: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  alcoholContent: PropTypes.number.isRequired
-}
+  alcoholContent: PropTypes.number.isRequired,
+  stock: PropTypes.number.isRequired,
+  id: PropTypes.string
+};
