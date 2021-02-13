4c60e940d52706c76ad93dc371b17aab857d3e5e
Austin Schrader <austin.jschrader@gmail.com>
2021-02-12 19:56:53 -0800

    Update keg detail output

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,1 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function KegDetail(props) {
+  
+}
+
+return (
+  <>
+    <h2>Keg Information Page</h2>
+    <h4>{keg.name}</h4>
+    <h4>{keg.brand}</h4>
+    <h4>{keg.price}</h4>
+    <h4>{keg.alcoholContent}</h4>
+    <h4>{keg.stock}</h4>
+    <h4>{keg.id}</h4>
+  </>
+)
+
