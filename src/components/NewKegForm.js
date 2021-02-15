da54db4398214e0c048f9738c9000947d75be24b
Austin Schrader <austin.jschrader@gmail.com>
2021-02-14 23:50:56 -0800

    Update form fields and add handler

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
 
 function NewKegForm(props) {
   return (
     <>
-      <form onSubmt={handleNewKegFormSubmission}>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input type='text' name='name' placeholder='Keg name' required />
+        <input type='text' name='brand' placeholder='Keg name' required />
+        <input type='text' name='price' placeholder='Keg name' required />
         <input
           type='text'
-          name='name'
+          name='alcoholContent'
           placeholder='Keg name'
-          required />
+          required
+        />
+        <input type='text' name='quantity' placeholder='Keg name' required />
       </form>
     </>
-  )
-};
+  );
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      name: event.target.name.value,
+      brand: event.target.name.brand,
+      price: event.target.name.price,
+      alcoholContent: event.target.name.alcoholContent,
+      quantity: event.target.name.quantity,
+      id: v4(),
+    });
+  }
+}
 
 NewKegForm.propTypes = {
-  onNewKegCreation: PropTypes.func
+  onNewKegCreation: PropTypes.func,
 };
 
 export default NewKegForm;
