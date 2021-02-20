02188db946232c1ea6906c54d7a00800c63da245
Austin Schrader <austin.jschrader@gmail.com>
2021-02-20 11:18:27 -0800

    Add action in preparation for 2nd good fail test

--- src/__tests__/keg-list-reducer.test.js
+++ src/__tests__/keg-list-reducer.test.js
@@ -4,4 +4,14 @@ describe('kegListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  let action;
+  const kegData = {
+    name: 'Orange Beer',
+    brand: 'Heff',
+    price: '2',
+    alcoholContent: 5,
+    quantity: 2,
+    id: 1,
+  };
 });
