fb72d25dc76bcfd0f23596c47286c7810888f32e
Austin Schrader <austin.jschrader@gmail.com>
2021-02-20 11:55:14 -0800

    Add second redux test to add new keg data

--- src/__tests__/keg-list-reducer.test.js
+++ src/__tests__/keg-list-reducer.test.js
@@ -1,17 +1,41 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
+let action;
+const kegData = {
+  name: 'Orange Beer',
+  brand: 'Heff',
+  price: '2',
+  alcoholContent: 5,
+  quantity: 2,
+  id: 1,
+};
+
 describe('kegListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
 
-  let action;
-  const kegData = {
-    name: 'Orange Beer',
-    brand: 'Heff',
-    price: '2',
-    alcoholContent: 5,
-    quantity: 2,
-    id: 1,
-  };
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, alcoholContent, quantity, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      quantity: quantity,
+      id: id,
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        quantity: quantity,
+        id: id,
+      },
+    });
+  });
 });
