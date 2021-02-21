c6e5a0e3e1379a2c312c7c65d6e9d60499584178
Austin Schrader <austin.jschrader@gmail.com>
2021-02-20 18:09:28 -0800

    Update test to include necessary data

--- src/__tests__/keg-list-reducer.test.js
+++ src/__tests__/keg-list-reducer.test.js
@@ -1,19 +1,19 @@
 import kegListReducer from '../reducers/reducers/keg-list-reducer';
 
-let action;
-const kegData = {
-  name: 'Orange Beer',
-  brand: 'Heff',
-  price: '2',
-  alcoholContent: 5,
-  quantity: 2,
-  id: 1,
-};
-
 describe('kegListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  let action;
+  const kegData = {
+    name: 'Orange Beer',
+    brand: 'Heff',
+    price: '2',
+    alcoholContent: 5,
+    quantity: 2,
+    id: 1,
+  };
 
   test('Should successfully add new keg data to masterKegList', () => {
     const { name, brand, price, alcoholContent, quantity, id } = kegData;
