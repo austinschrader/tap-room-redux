0a9301d61054e2b90625a1af46897f0f8fc60279
Austin Schrader <austin.jschrader@gmail.com>
2021-02-20 19:17:13 -0800

    Import reducer to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
 
 ReactDOM.render(
   <React.StrictMode>
