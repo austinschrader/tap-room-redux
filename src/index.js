6c42046004bf26ae80c7edc186e267a3ef78a100
Austin Schrader <austin.jschrader@gmail.com>
2021-02-20 19:17:34 -0800

    Add redux store and pass in reducer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
