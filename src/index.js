f214d4838647eb11dcfab08b09acc74444a19f33
Austin Schrader <austin.jschrader@gmail.com>
2021-02-20 19:20:21 -0800

    Add the provider component and wrap it around app component

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import { Provider } from 'react-redux';
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
